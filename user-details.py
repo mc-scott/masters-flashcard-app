195c09e89f836824744700ba78e1540978cc8a1c
Matthew Scott <msmattscott@gmail.com>
2026-04-12 15:41:45 +0100

    updating app config and venv setup

--- user-details.py
+++
@@ -1,9 +0,0 @@
-from posit import connect
-
-client = connect.Client()
-
-# Find all users (includes guid in results)
-users = client.users.find()
-
-# Or find by prefix (username, first name, or last name)
-users = client.users.find(prefix="mc-scott")
